d4df7863417e0982f2a69841cb782322c523baa7
Abdullah149081 <masudmd425@gmail.com>
2023-03-22 19:55:32 +0600

    img add

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./Country.css";
 
 const Country = (props) => {
-  const { name, capital, population, area } = props.country;
+  const { name, capital, population, area, flags } = props.country;
   // console.log(props.country);
   return (
     <div className="country-details">
+      <img className="img-country" src={flags.png} alt={flags.png} />
       <h2> Country Name: {name.common}</h2>
       <p> Capital: {capital}</p>
       <p> Population: {population}</p>
